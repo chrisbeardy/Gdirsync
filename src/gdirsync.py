81583b001651810f0d36b1a867c2fcd795685a7f
chrisbeardy <20585410+chrisbeardy@users.noreply.github.com>
2019-12-30 14:25:49 +0000

    changed to use multi processing, popup happens on main thread/process as tkinter doesnt like bing called not on main

--- src/gdirsync.py
+++ src/gdirsync.py
@@ -1,15 +1,14 @@
 import PySimpleGUI as sg
-import threading
+import multiprocessing
 import dirsync
 import sys
 
 
-class ExcThread(threading.Thread):
-    """Class to extend the python threading module."""
+class ExcProcess(multiprocessing.Process):
+    """Class to extend the python processing module."""
 
     def exc_run(self):
-        """Overwrite this method in implementation.
-        """
+        """Overwrite this method in implementation."""
         pass
 
     def run(self):
@@ -22,26 +21,19 @@ class ExcThread(threading.Thread):
             # Save details of the exception thrown but don't rethrow,
             # just complete the function
 
-    def join(self):
-        threading.Thread.join(self)
-        if self.exc:
-            msg = "Thread '%s' threw an exception: %s" % (self.getName(), self.exc[1])
-            new_exc = Exception(msg)
-            raise new_exc.with_traceback(self.exc[2])
 
-
-class GSync(ExcThread):
+class GSync(ExcProcess):
     """Performs the sync action using dirsync.
 
-    :param source_dir: source directory to sync from
-    :type source_dir: str
-    :param target_dir: target directory to sync to
-    :type target_dir: str
-    :param action: action to perform, e.g. 'sync'
-    :type action: str
-    :param options: additional options to pass to dirsync, e.g. purge = True
+   :param source_dir: source directory to sync from
+   :type source_dir: str
+   :param target_dir: target directory to sync to
+   :type target_dir: str
+   :param action: action to perform, e.g. 'sync'
+   :type action: str
+   :param options: additional options to pass to dirsync, e.g. purge = True
 
-    """
+   """
 
     def __init__(self, source_dir, target_dir, action, **options):
         super(GSync, self).__init__()
@@ -57,7 +49,6 @@ class GSync(ExcThread):
 def main():
     """Entry point for gdirsync."""
     # PySimpleGUI Layout
-    progress_bar_len = 1000
     layout = [
         [
             sg.Text("Source Directory", size=(20, 1)),
@@ -83,37 +74,29 @@ def main():
             ),
         ],
         [sg.Button("Sync", key="syncButton")],
-        [
-            sg.ProgressBar(
-                progress_bar_len, orientation="h", size=(50, 20), key="progressBar"
-            )
-        ],
     ]
     window = sg.Window("Gdirsync").Layout(layout)
     source_dir_browse = window["sourceDirBrowse"]
     target_dir_browse = window["targetDirBrowse"]
     sync_button = window["syncButton"]
-    progress_bar = window["progressBar"]
-    syncing = False
 
-    def reset_main_window(syncing, progress_loop):
+    def reset_main_window():
         """Internal function to reset main window elements."""
         source_dir_browse.update(disabled=False)
         target_dir_browse.update(disabled=False)
         sync_button.update("Sync")
         sync_button.update(disabled=False)
-        syncing = False
-        progress_loop = 0
-        progress_bar.UpdateBar(progress_loop)
-        return syncing, progress_loop
 
     # App Logic
-    progress_loop = 0
+    sync_jobs = []
+    source_dirs = []
+    target_dirs = []
+    completed_jobs = []
     while True:
-        event, values = window.read(timeout=100)
+        event, values = window.read(timeout=500)
         if event is None:
             break
-        elif not syncing and event == "syncButton":
+        elif event == "syncButton":
             source_dir = values[0]
             target_dir = values[1]
             if source_dir is "" or target_dir is "":
@@ -121,27 +104,29 @@ def main():
                 target_dir_browse.update(disabled=True)
                 sync_button.update(disabled=True)
                 sg.Popup("Please complete all fields")
-                syncing, progress_loop = reset_main_window(syncing, progress_loop)
+                reset_main_window()
             else:
-                t1 = GSync(
-                    source_dir, target_dir, "sync", purge=values[2], create=values[3]
-                )
-                t1.start()
-                syncing = True
-                source_dir_browse.update(disabled=True)
-                target_dir_browse.update(disabled=True)
-                sync_button.update(disabled=True)
-        elif syncing and not t1.is_alive():
-            t1.join()
-            sg.Popup("Sync Complete")
-            syncing, progress_loop = reset_main_window(syncing, progress_loop)
-        elif syncing and event == sg.TIMEOUT_KEY:
-            progress_bar.UpdateBar(progress_loop)
-            progress_loop += 1
-            if progress_loop >= progress_bar_len:
-                progress_loop = 1
-    if syncing:
-        t1.join()
+                p = GSync(
+                        source_dir,
+                        target_dir,
+                        "sync",
+                        purge=values[2],
+                        create=values[3],
+                    )
+                sync_jobs.append(p)
+                source_dirs.append(source_dir)
+                target_dirs.append(target_dir)
+                completed_jobs.append(False)
+                p.start()
+                sg.Popup("Syncing " + source_dir + " to " + target_dir, title="Syncing...", auto_close=True, auto_close_duration=2, non_blocking=True)
+
+        for i in range(len(sync_jobs)):
+            if not sync_jobs[i].is_alive() and not completed_jobs[i]:
+                sg.Popup("Sync from " + source_dirs[i] + " to " + target_dirs[i] + " complete ", title="Sync Complete", non_blocking=True)
+                completed_jobs[i] = True
+
+    for j in sync_jobs:
+        j.join()
 
 
 if __name__ == "__main__":
