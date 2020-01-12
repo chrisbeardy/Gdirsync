7e5c6053ea3192cc6b0b0f26bf378647c0787da9
chrisbeardy <20585410+chrisbeardy@users.noreply.github.com>
2020-01-12 16:51:03 +0000

    adding stuuf ready for first publish

---
+++ tests/test_gdirsync.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class gdirsyncTest(unittest.TestCase):
+    """Unittests for gdirsync"""
+
+    def sample_test(self):
+        """sample test"""
+        a = 5
+        self.assertEqual(a, 5)
+
+tox
+if __name__ == "__main__":
+    unittest.main()
